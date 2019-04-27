70794b4fe8386e2fa8afa96699fdb1dbba1f70ad
John Goldhamer <johng@MacBook-Pro.local>
2019-04-27 13:47:26 -0400

    have done a lot with the quoted lines but still have: error reporting fields with line returns in them last field in row looping inf or missing blank

--- beta.py
+++ beta.py
@@ -4,21 +4,44 @@
 
 def load(fileName, para):
 
-    # load the csv file
-    csvFile = open(fileName, "r")
+    # NOTE: Using the RFC 4180 CSV Spec
+
+    with open(fileName) as csvFile:
+
+        # NOTE: Spec does not require headers
+
+
+        for line in csvFile:
+
+            if line.__contains__("\""):
+                print("line contains double quotes")
+
+                processQuotedLine(line)
+
+
+            else:
+                print("not quotes in line")
+
+                rows = line.split(",")
+                print(rows)
+                print(rows.__len__())
 
 
 
 
 
-    print(csvFile.readline())
+
+
+
+    # TODO: check column length after loading
+
 
 
     print("para = ", para)
 
 
 
-    # Using the RFC 4180 CSV Spec
+
     # start parsing lines
     # make sure to NOT trim spaces out
     # make sure to
@@ -33,4 +56,32 @@ def load(fileName, para):
     #   make sure windows and unix style new lines are accepted (new line delimination)
 
 
+# Check if each line has the same number of columns by ______
+# def checkValidColumnLength(csvFile):
+    # by checking number of commas in each line?
+    #  but this might ignore escaped commas
 
+    # OR
+
+def processQuotedLine(line):
+    while line.__len__() > 0:
+
+        # field starts with
+        if line[0] == "\"":
+            next_quote = line.find("\"")
+            print(next_quote,1)
+
+            # find next quote
+
+            # check if escaped quote
+            # if yes, then find next and get rid of the escaped ones recursively then save field
+            # if no then save field
+
+        # detect valid quotes
+        # detect escaped quotes and replace
+        # detect escaped commas
+
+
+def logError(str):
+    print("ERROR: ", logError)
+    exit()
